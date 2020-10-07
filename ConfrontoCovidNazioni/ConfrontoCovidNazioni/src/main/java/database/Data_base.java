3a5c514118de3d78ce91c0a3fd35e2c27daee08c
cervoz <cervoz@172.23.158.158>
2020-10-07 16:29:22 +0200

    data and metadata creation

--- ConfrontoCovidNazioni/ConfrontoCovidNazioni/src/main/java/database/Data_base.java
+++ ConfrontoCovidNazioni/ConfrontoCovidNazioni/src/main/java/database/Data_base.java
@@ -80,7 +80,6 @@ public class Data_base  {
 	
 	
 	
-	
 URL sveziaURL = new URL("https://api.covid19api.com/country/sweden?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z");
 URLConnection URLConnsv= sveziaURL.openConnection();
 InputStream input2=URLConnsv.getInputStream();
