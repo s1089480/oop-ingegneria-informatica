14d61b826f0669f5706291af25e6cc695dbc595e
cervoz <cervoz@172.23.158.158>
2020-10-08 16:53:02 +0200

    /metadati ora funziona

--- ConfrontoCovidNazioni/ConfrontoCovidNazioni/src/main/java/Statistiche/covid/ConfrontoCovidNazioni/controller/SimpleController.java
+++ ConfrontoCovidNazioni/ConfrontoCovidNazioni/src/main/java/Statistiche/covid/ConfrontoCovidNazioni/controller/SimpleController.java
@@ -1,8 +1,9 @@
-package controller;
+package Statistiche.covid.ConfrontoCovidNazioni.controller;
 
 import java.util.ArrayList;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,10 +19,10 @@ public class SimpleController {
 
 	
 	@RequestMapping(value = "/metadati", method=RequestMethod.GET)
-	public ArrayList<Metadati> getMetadata(){
+	public ResponseEntity<ArrayList<Metadati>> getMetadata(){
 		
-		return Data_base.getArrayMetadati();
-		
+		//return Data_base.getArrayMetadati();
+		return new ResponseEntity<ArrayList<Metadati>>(Data_base.getArrayMetadati(), HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/dati", method=RequestMethod.GET)
